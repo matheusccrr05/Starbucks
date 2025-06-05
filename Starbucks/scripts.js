e2518580f5b2571691cca77387a29a371682d59a
MATHEUS CAMPOS RIBEIRO <matheusccrr05@gmail.com>
2025-06-05 12:24:34 -0300

    Pagina simples starbucks

---
+++ Starbucks/scripts.js
@@ -0,0 +1,9 @@
+
+let circulo = document.querySelector(".circulo")
+let copo = document.querySelector(".imgG img")
+
+function TrocarCorImg(cor, imagem){
+    circulo.style.background = cor
+    copo.src = imagem
+
+}
